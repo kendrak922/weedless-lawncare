9d3e28cad06f3a878721bedfa4fde2900d442d14
Kendra Kyndberg <kendrak922@gmail.com>
2021-01-19 23:03:22 -0600

    fixed sass bug

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
-import './App.scss';
+import './global.scss';
+import Hero from './Components/Hero/hero'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Hero />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +14,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
---
+++ src/Components/Hero/hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './hero.scss'
+
+const Hero = ()=>{
+
+return(
+    <div className="hero">
+    <h1>Weedless Lawn Care</h1>
+    </div>
+)
+}
+
+export default Hero;
